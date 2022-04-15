43f39b2d05c45ecc132cd4de2735653b578b3ab0
Sagar Sharma <tech.sagarsh@gmail.com>
2022-04-15 19:20:16 +0530

    code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const scoreCount = document.getElementById('scores');
 
 const quizDataBase = [
     {
-        question: "What does HTML stand for?",
+        question: "Q.1) What does HTML stand for?",
         0: "Hyperlinks and Text Markup Language", 
         1: "Hyper Text Markup Language", 
         2: "Home Tool Markup Language", 
@@ -15,7 +15,7 @@ const quizDataBase = [
         ans: "1"
     },
      {
-        question: "Choose the correct HTML tag for the largest heading",
+        question: "Q.2) Choose the correct HTML tag for the largest heading",
         0: "&lt;heading&gt;", 
         1: "&lt;h6&gt;", 
         2: "&lt;head&gt;", 
@@ -23,7 +23,7 @@ const quizDataBase = [
         ans: "1"
     },
      {
-        question: "What is the correct HTML tag for inserting a line break?",
+        question: "Q.3) What is the correct HTML tag for inserting a line break?",
         0: "&lt;br&gt;", 
         1: "&lt;lb&gt;", 
         2: "&lt;break&gt;", 
@@ -31,7 +31,7 @@ const quizDataBase = [
         ans: "0"
     } ,
     {
-        question: "What is the correct HTML for creating a hyperlink?",
+        question: "Q.4) What is the correct HTML for creating a hyperlink?",
         0: "&lt;a url=\"http:www.w3schools.com\">W3Schools.com&lt;/a&gt;", 
         1: "&lt;a name=\"http://www.w3schools.com\">W3Schools.com&lt;/a&gt;", 
         2: "&lt;a href=\"http://www.w3schools.com\">W3Schools&lt;/a&gt;", 
@@ -39,7 +39,7 @@ const quizDataBase = [
         ans: "2"
     },
     {
-        question: "How can you make a numbered list?",
+        question: "Q.5) How can you make a numbered list?",
         0: "&lt;ol&gt;", 
         1: "&lt;ul&gt;", 
         2: "&lt;dl&gt;", 
